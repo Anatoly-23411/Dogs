7fb15ffee6b3f86fe2b44cd5dace62628ffb7182
Anatoly-23411 <popov234111@gmail.com>
2024-09-03 17:10:37 +0500

    Создана функция загрузки ссылки на изображение

--- Dogs.py
+++ Dogs.py
@@ -9,6 +9,20 @@ from bottle import response
 from pygame.examples.cursors import image
 
 
+def get_dog_image():
+    try:
+        response = requests.get("https://dog.ceo/api/breeds/image/random")
+        response.raise_for_status()
+        data.response.json()
+        return data("message")
+    except Exception as e:
+        mb.showerror("Ошибка", f"Возникла ошибка при запросе к API {e}")
+        return None
+
+
+
+
+
 def show_image():
     image_url = get_dog_image()
     if image_url:
@@ -18,10 +32,11 @@ def show_image():
             img.data = BytesIO(response.content)
             img = Image.open(img.data)
             img.thumbnail(300, 300)
+            img = ImageTk.PhotoImage(img)
             label.config(image=img)
             label.image = img
         except Exception as e:
-            mb.showerror("Ошибка", f"Возникла ошибка {e}")
+            mb.showerror("Ошибка", f"Возникла ошибка при загрузке изображения {e}")
 
 
 
